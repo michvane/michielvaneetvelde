0346a4d3dcc59fe754bb01f607053a68bf0b0094
Michiel <michiel.vaneetvelde@tangent.works>
2026-06-28 13:42:32 +0200

    Update game

--- public/game/Build/web-mobile.loader.js
+++ public/game/Build/web-mobile.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 63857937,
+    wasmFileSize: 63894515,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
